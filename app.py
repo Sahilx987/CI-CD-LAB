aa77e5903df3ecf807ff450e0cc974b8a9fdfa77
root <root@ip-172-31-41-64.ap-south-1.compute.internal>
2026-04-28 16:19:59 +0000

    webhook test from EC2

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route('/health', methods=['GET'])
 def health():
-    return jsonify({"status": "ok"})
+    return return "Webhook Working 🚀"
 
 @app.route('/jobs', methods=['GET'])
 def get_jobs():
