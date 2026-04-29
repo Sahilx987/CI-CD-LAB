c1fa8bfab136750dfb22fbbd7ae0ed47e225bf93
root <root@ip-172-31-41-64.ap-south-1.compute.internal>
2026-04-29 08:32:54 +0000

    sucessfull

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route('/health', methods=['GET'])
 def health():
-    return "Webhook Working 🚀 CI/CD working V1  "
+    return "Webhook Working 🚀   "
 
 @app.route('/jobs', methods=['GET'])
 def get_jobs():
